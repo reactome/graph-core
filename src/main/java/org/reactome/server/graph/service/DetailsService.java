14cdd9d4ff14bc651cc4405f9fc1129805957e10
gviteri <gviteri@ebi.ac.uk>
2016-06-23 11:40:19 +0100

    Merging aspectj branch

--- src/main/java/org/reactome/server/graph/service/DetailsService.java
+++ src/main/java/org/reactome/server/graph/service/DetailsService.java
@@ -10,8 +10,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Set;
 
--- src/test/java/org/reactome/server/graph/service/ComponentServiceTest.java
+++ src/test/java/org/reactome/server/graph/service/ComponentServiceTest.java
@@ -6,23 +6,15 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.reactome.server.graph.config.Neo4jConfig;
-import org.reactome.server.graph.domain.model.DatabaseObject;
-import org.reactome.server.graph.domain.model.PhysicalEntity;
-import org.reactome.server.graph.domain.model.ReferenceEntity;
 import org.reactome.server.graph.domain.result.ComponentOf;
 import org.reactome.server.graph.util.DatabaseObjectFactory;
-import org.reactome.server.graph.util.JunitHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assume.assumeTrue;
