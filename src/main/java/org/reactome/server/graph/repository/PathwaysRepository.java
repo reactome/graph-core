e8a04459142b51e62ba26de9f332079f8bd77d3a
Antonio Fabregat <fabregat.antonio@gmail.com>
2017-02-28 10:48:58 +0000

    diagram flagging queries improvement

--- src/main/java/org/reactome/server/graph/repository/PathwaysRepository.java
+++ src/main/java/org/reactome/server/graph/repository/PathwaysRepository.java
@@ -99,24 +99,27 @@ public interface PathwaysRepository extends GraphRepository<DatabaseObject> {
     Collection<SimpleDatabaseObject> getPathwaysWithDiagramForAllFormsOfByDbId(Long dbId, Long speciesId);
 
 
-    @Query(" MATCH (rd:ReferenceDatabase)<--(n{identifier:{0}})<-[:referenceEntity|referenceSequence|crossReference|referenceGene*]-(pe:PhysicalEntity) " +
+    @Query(" MATCH (rd:ReferenceDatabase)<--(n)<-[:referenceEntity|referenceSequence|crossReference|referenceGene*]-(pe:PhysicalEntity) " +
+            "WHERE n.identifier = {0} OR {0} IN n.name OR {0} IN n.geneName " +
             "WITH DISTINCT pe " +
             "MATCH (pe)<-[:regulatedBy|regulator|physicalEntity|entityFunctionalStatus|catalystActivity|hasMember|hasCandidate|hasComponent|repeatedUnit|input|output*]-(:ReactionLikeEvent)<-[:hasEvent]-(p:Pathway)-[:species]->(s:Species{dbId:{1}}) " +
             "RETURN Distinct(p.dbId) as dbId, p.stId as stId, p.displayName as displayName, labels(p) as labels")
     Collection<SimpleDatabaseObject> getLowerLevelPathwaysForIdentifier(String identifier, Long speciesId);
 
 
-    @Query(" MATCH (rd:ReferenceDatabase)<--(n{identifier:{0}})<-[:referenceEntity|referenceSequence|crossReference|referenceGene*]-(pe:PhysicalEntity) " +
-            "WITH DISTINCT pe " +
-            "MATCH (pe)<-[:regulatedBy|regulator|physicalEntity|entityFunctionalStatus|catalystActivity|hasMember|hasCandidate|hasComponent|repeatedUnit|input|output|hasEvent*]-(p:Pathway) " +
-            "WHERE p.stId IN {1} " +
+    @Query(" MATCH (pe)<-[:regulatedBy|regulator|physicalEntity|entityFunctionalStatus|catalystActivity|hasMember|hasCandidate|hasComponent|repeatedUnit|input|output|hasEvent*]-(p:Pathway) " +
+            "WHERE p.stId IN [{1}] " +
+            "WITH DISTINCT p, pe " +
+            "MATCH (rd:ReferenceDatabase)<--(n)<-[:referenceEntity|referenceSequence|crossReference|referenceGene*]-(pe:PhysicalEntity) " +
+            "WHERE n.identifier = {0} OR {0} IN n.name OR {0} IN n.geneName " +
             "RETURN Distinct(p.dbId) as dbId, p.stId as stId, p.displayName as displayName, labels(p) as labels")
     Collection<SimpleDatabaseObject> getPathwaysForIdentifierByStId(String identifier, Collection<String> pathways);
 
-    @Query(" MATCH (rd:ReferenceDatabase)<--(n{identifier:{0}})<-[:referenceEntity|referenceSequence|crossReference|referenceGene*]-(pe:PhysicalEntity) " +
-            "WITH DISTINCT pe " +
-            "MATCH (pe)<-[:regulatedBy|regulator|physicalEntity|entityFunctionalStatus|catalystActivity|hasMember|hasCandidate|hasComponent|repeatedUnit|input|output|hasEvent*]-(p:Pathway) " +
-            "WHERE p.dbId IN {1} " +
+    @Query(" MATCH (pe)<-[:regulatedBy|regulator|physicalEntity|entityFunctionalStatus|catalystActivity|hasMember|hasCandidate|hasComponent|repeatedUnit|input|output|hasEvent*]-(p:Pathway) " +
+            "WHERE p.dbId IN [{1}] " +
+            "WITH DISTINCT p, pe " +
+            "MATCH (rd:ReferenceDatabase)<--(n)<-[:referenceEntity|referenceSequence|crossReference|referenceGene*]-(pe:PhysicalEntity) " +
+            "WHERE n.identifier = {0} OR {0} IN n.name OR {0} IN n.geneName " +
             "RETURN Distinct(p.dbId) as dbId, p.stId as stId, p.displayName as displayName, labels(p) as labels")
     Collection<SimpleDatabaseObject> getPathwaysForIdentifierByDbId(String identifier, Collection<Long> pathways);
 
@@ -128,7 +131,8 @@ public interface PathwaysRepository extends GraphRepository<DatabaseObject> {
             "UNWIND ps AS p " +
             "MATCH (p)-[:hasEvent]->(rle:ReactionLikeEvent) " +
             "WITH DISTINCT rle " +
-            "MATCH (rd:ReferenceDatabase)<--(n{identifier:{1}})<-[:referenceEntity|referenceSequence|crossReference|referenceGene|hasComponent|hasMember|hasCandidate|repeatedUnit*]-(pe)<-[:input|output|catalystActivity|entityFunctionalStatus|physicalEntity|regulatedBy|regulator*]-(rle) " +
+            "MATCH (rd:ReferenceDatabase)<--(n)<-[:referenceEntity|referenceSequence|crossReference|referenceGene|hasComponent|hasMember|hasCandidate|repeatedUnit*]-(pe)<-[:input|output|catalystActivity|entityFunctionalStatus|physicalEntity|regulatedBy|regulator*]-(rle) " +
+            "WHERE n.identifier = {1} OR {1} IN n.name OR {1} IN n.geneName " +
             "RETURN DISTINCT(pe.dbId) as dbId, pe.stId as stId, pe.displayName as displayName, labels(pe) as labels")
     Collection<SimpleDatabaseObject> getDiagramEntitiesForIdentifierByStId(String stId, String identifier);
 
@@ -139,7 +143,8 @@ public interface PathwaysRepository extends GraphRepository<DatabaseObject> {
             "UNWIND ps AS p " +
             "MATCH (p)-[:hasEvent]->(rle:ReactionLikeEvent) " +
             "WITH DISTINCT rle " +
-            "MATCH (rd:ReferenceDatabase)<--(n{identifier:{1}})<-[:referenceEntity|referenceSequence|crossReference|referenceGene|hasComponent|hasMember|hasCandidate|repeatedUnit*]-(pe)<-[:input|output|catalystActivity|entityFunctionalStatus|physicalEntity|regulatedBy|regulator*]-(rle) " +
+            "MATCH (rd:ReferenceDatabase)<--(n)<-[:referenceEntity|referenceSequence|crossReference|referenceGene|hasComponent|hasMember|hasCandidate|repeatedUnit*]-(pe)<-[:input|output|catalystActivity|entityFunctionalStatus|physicalEntity|regulatedBy|regulator*]-(rle) " +
+            "WHERE n.identifier = {1} OR {1} IN n.name OR {1} IN n.geneName " +
             "RETURN DISTINCT(pe.dbId) as dbId, pe.stId as stId, pe.displayName as displayName, labels(pe) as labels")
     Collection<SimpleDatabaseObject> getDiagramEntitiesForIdentifierByDbId(Long dbId, String identifier);
 }
