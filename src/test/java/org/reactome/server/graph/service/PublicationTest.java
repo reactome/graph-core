c35a3206e66545ff6dde90336b3eb9349964d9cf
Antonio Fabregat <fabregat.antonio@gmail.com>
2017-03-26 12:08:03 +0100

    publication test added. it tests the order of the authors

---
+++ src/test/java/org/reactome/server/graph/service/PublicationTest.java
@@ -0,0 +1,50 @@
+package org.reactome.server.graph.service;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.reactome.server.graph.domain.model.Person;
+import org.reactome.server.graph.domain.model.Publication;
+import org.reactome.server.graph.util.DatabaseObjectFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Objects;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author Antonio Fabregat <fabregat@ebi.ac.uk>
+ */
+public class PublicationTest extends BaseTest {
+
+    @Autowired
+    private DatabaseObjectService databaseObjectService;
+
+    @BeforeClass
+    public static void setUpClass() {
+        logger.info(" --- !!! Running " + PublicationTest.class.getName() + " !!! --- \n");
+    }
+
+    @Test
+    public void findAuthorsList() {
+        logger.info("Started testing publicationTest.findAuthorsList");
+
+        long start, time;
+        start = System.currentTimeMillis();
+        Publication publicationGDB = databaseObjectService.findById("6799811");
+        Publication publicationRDB = DatabaseObjectFactory.createObject("6799811");
+
+        assertTrue("Different sizes", Objects.equals(publicationGDB.getAuthor().size(), publicationRDB.getAuthor().size()));
+
+        for (int i = 0; i < publicationRDB.getAuthor().size(); i++) {
+            Person pRDB = publicationRDB.getAuthor().get(i);
+            Person pGDB = publicationGDB.getAuthor().get(i);
+            assertTrue(pGDB + " differs of " + pRDB, Objects.equals(pRDB, pGDB));
+        }
+
+        time = System.currentTimeMillis() - start;
+        logger.info("Comparison execution time: " + time + "ms");
+
+        logger.info("Finished");
+    }
+
+}
