b235e4d3c8abf97229db38ea7d3fd2eda48661d9
Antonio Fabregat <fabregat.antonio@gmail.com>
2018-01-22 17:05:47 +0000

    interaction classes added

---
+++ src/main/java/org/reactome/server/graph/domain/model/Interaction.java
@@ -0,0 +1,69 @@
+package org.reactome.server.graph.domain.model;
+
+import org.neo4j.ogm.annotation.NodeEntity;
+import org.neo4j.ogm.annotation.Relationship;
+import org.reactome.server.graph.domain.annotations.ReactomeProperty;
+
+import java.util.List;
+
+@SuppressWarnings({"unused", "WeakerAccess"})
+@NodeEntity
+public abstract class Interaction extends DatabaseObject {
+
+    @Relationship(type = "referenceDatabase")
+    private ReferenceDatabase referenceDatabase;
+    @ReactomeProperty
+    private String databaseName;
+    @ReactomeProperty
+    private Double score;
+    @ReactomeProperty
+    private List<String> accession;
+    @ReactomeProperty
+    private String url;
+
+//    private List<String> type;
+//    private String expansionType; //spokeExpansion
+
+    public Interaction() { }
+
+    public ReferenceDatabase getReferenceDatabase() {
+        return referenceDatabase;
+    }
+
+    @Relationship(type = "referenceDatabase")
+    public void setReferenceDatabase(ReferenceDatabase referenceDatabase) {
+        this.referenceDatabase = referenceDatabase;
+    }
+
+    public String getDatabaseName() {
+        return databaseName;
+    }
+
+    public void setDatabaseName(String databaseName) {
+        this.databaseName = databaseName;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
+    }
+
+    public List<String> getAccession() {
+        return accession;
+    }
+
+    public void setAccession(List<String> accession) {
+        this.accession = accession;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
