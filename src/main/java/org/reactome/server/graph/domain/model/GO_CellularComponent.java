ee6e1dedb51664ec85e1a176cfb1d9fa0e7239c0
Antonio Fabregat <fabregat.antonio@gmail.com>
2018-01-22 17:01:02 +0000

    missing fields added

--- src/main/java/org/reactome/server/graph/domain/model/GO_CellularComponent.java
+++ src/main/java/org/reactome/server/graph/domain/model/GO_CellularComponent.java
@@ -1,11 +1,49 @@
 package org.reactome.server.graph.domain.model;
 
 import org.neo4j.ogm.annotation.NodeEntity;
+import org.neo4j.ogm.annotation.Relationship;
+
+import java.util.List;
 
 @SuppressWarnings("unused")
 @NodeEntity
 public class GO_CellularComponent extends GO_Term {
 
+    @Relationship(type = "componentOf")
+    private List<GO_CellularComponent> componentOf;
+
+    @Relationship(type = "hasPart")
+    private List<GO_CellularComponent> hasPart;
+
+    @Relationship(type = "instanceOf")
+    private List<GO_CellularComponent> instanceOf;
+
     public GO_CellularComponent() {}
 
+    public List<GO_CellularComponent> getComponentOf() {
+        return componentOf;
+    }
+
+    @Relationship(type = "componentOf")
+    public void setComponentOf(List<GO_CellularComponent> componentOf) {
+        this.componentOf = componentOf;
+    }
+
+    public List<GO_CellularComponent> getHasPart() {
+        return hasPart;
+    }
+
+    @Relationship(type = "hasPart")
+    public void setHasPart(List<GO_CellularComponent> hasPart) {
+        this.hasPart = hasPart;
+    }
+
+    public List<GO_CellularComponent> getInstanceOf() {
+        return instanceOf;
+    }
+
+    @Relationship(type = "instanceOf")
+    public void setInstanceOf(List<GO_CellularComponent> instanceOf) {
+        this.instanceOf = instanceOf;
+    }
 }
--- src/main/java/org/reactome/server/graph/domain/model/GO_MolecularFunction.java
+++ src/main/java/org/reactome/server/graph/domain/model/GO_MolecularFunction.java
@@ -5,6 +5,8 @@ import org.neo4j.ogm.annotation.Relationship;
 import org.reactome.server.graph.domain.annotations.ReactomeAllowedClasses;
 import org.reactome.server.graph.domain.annotations.ReactomeProperty;
 
+import java.util.List;
+
 @SuppressWarnings("unused")
 @NodeEntity
 public class GO_MolecularFunction extends GO_Term {
@@ -13,17 +15,27 @@ public class GO_MolecularFunction extends GO_Term {
     private String ecNumber;
 
     @Relationship(type = "componentOf")
-    @ReactomeAllowedClasses(allowed = {GO_MolecularFunction.class, GO_BiologicalProcess.class})
-    private DatabaseObject componentOf;
+    @ReactomeAllowedClasses(allowed = {GO_BiologicalProcess.class, GO_MolecularFunction.class})
+    private List<GO_Term> componentOf;
+
+    @Relationship(type = "hasPart")
+    @ReactomeAllowedClasses(allowed = {GO_BiologicalProcess.class, GO_MolecularFunction.class})
+    private List<GO_Term> hasPart;
+
+    @Relationship(type = "instanceOf")
+    private List<GO_MolecularFunction> instanceOf;
 
     @Relationship(type = "negativelyRegulate")
-    private GO_MolecularFunction negativelyRegulate;
+    @ReactomeAllowedClasses(allowed = {GO_BiologicalProcess.class, GO_MolecularFunction.class})
+    private List<GO_Term> negativelyRegulate;
 
     @Relationship(type = "positivelyRegulate")
-    private GO_MolecularFunction positivelyRegulate;
+    @ReactomeAllowedClasses(allowed = {GO_BiologicalProcess.class, GO_MolecularFunction.class})
+    private List<GO_Term> positivelyRegulate;
 
     @Relationship(type = "regulate")
-    private GO_MolecularFunction regulate;
+    @ReactomeAllowedClasses(allowed = {GO_BiologicalProcess.class, GO_MolecularFunction.class})
+    private List<GO_Term> regulate;
 
     public GO_MolecularFunction() {}
 
@@ -36,40 +48,62 @@ public class GO_MolecularFunction extends GO_Term {
         this.ecNumber = ecNumber;
     }
 
-    @ReactomeAllowedClasses(allowed = {GO_MolecularFunction.class, GO_BiologicalProcess.class})
-    public DatabaseObject getComponentOf() {
+    @ReactomeAllowedClasses(allowed = {GO_BiologicalProcess.class, GO_MolecularFunction.class})
+    public List<GO_Term> getComponentOf() {
         return componentOf;
     }
 
     @Relationship(type = "componentOf")
-    public void setComponentOf(DatabaseObject componentOf) {
+    public void setComponentOf(List<GO_Term> componentOf) {
         this.componentOf = componentOf;
     }
 
-    public GO_MolecularFunction getNegativelyRegulate() {
+    @ReactomeAllowedClasses(allowed = {GO_BiologicalProcess.class, GO_MolecularFunction.class})
+    public List<GO_Term> getHasPart() {
+        return hasPart;
+    }
+
+    @Relationship(type = "hasPart")
+    public void setHasPart(List<GO_Term> hasPart) {
+        this.hasPart = hasPart;
+    }
+
+    public List<GO_MolecularFunction> getInstanceOf() {
+        return instanceOf;
+    }
+
+    @Relationship(type = "instanceOf")
+    public void setInstanceOf(List<GO_MolecularFunction> instanceOf) {
+        this.instanceOf = instanceOf;
+    }
+
+    @ReactomeAllowedClasses(allowed = {GO_BiologicalProcess.class, GO_MolecularFunction.class})
+    public List<GO_Term> getNegativelyRegulate() {
         return negativelyRegulate;
     }
 
     @Relationship(type = "negativelyRegulate")
-    public void setNegativelyRegulate(GO_MolecularFunction negativelyRegulate) {
+    public void setNegativelyRegulate(List<GO_Term> negativelyRegulate) {
         this.negativelyRegulate = negativelyRegulate;
     }
 
-    public GO_MolecularFunction getPositivelyRegulate() {
+    @ReactomeAllowedClasses(allowed = {GO_BiologicalProcess.class, GO_MolecularFunction.class})
+    public List<GO_Term> getPositivelyRegulate() {
         return positivelyRegulate;
     }
 
     @Relationship(type = "positivelyRegulate")
-    public void setPositivelyRegulate(GO_MolecularFunction positivelyRegulate) {
+    public void setPositivelyRegulate(List<GO_Term> positivelyRegulate) {
         this.positivelyRegulate = positivelyRegulate;
     }
 
-    public GO_MolecularFunction getRegulate() {
+    @ReactomeAllowedClasses(allowed = {GO_BiologicalProcess.class, GO_MolecularFunction.class})
+    public List<GO_Term> getRegulate() {
         return regulate;
     }
 
     @Relationship(type = "regulate")
-    public void setRegulate(GO_MolecularFunction regulate) {
+    public void setRegulate(List<GO_Term> regulate) {
         this.regulate = regulate;
     }
 }
