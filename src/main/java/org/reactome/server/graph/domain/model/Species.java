7b9980c124bd59c21e49c9712b96513646ad4c0e
Antonio Fabregat <fabregat.antonio@gmail.com>
2018-04-13 09:01:02 +0100

    changes in the model

--- src/main/java/org/reactome/server/graph/domain/model/Species.java
+++ src/main/java/org/reactome/server/graph/domain/model/Species.java
@@ -1,31 +1,16 @@
 package org.reactome.server.graph.domain.model;
 
 import org.neo4j.ogm.annotation.NodeEntity;
-import org.neo4j.ogm.annotation.Relationship;
 import org.reactome.server.graph.domain.annotations.ReactomeProperty;
-
-import java.util.List;
 
 @SuppressWarnings("unused")
 @NodeEntity
 public class Species extends Taxon {
 
-    @Relationship(type = "figure")
-    private List<Figure> figure;
-
     @ReactomeProperty
     private String abbreviation;
 
     public Species() {}
-
-    public List<Figure> getFigure() {
-        return figure;
-    }
-
-    @Relationship(type = "figure")
-    public void setFigure(List<Figure> figure) {
-        this.figure = figure;
-    }
 
     public String getAbbreviation() {
         return abbreviation;
