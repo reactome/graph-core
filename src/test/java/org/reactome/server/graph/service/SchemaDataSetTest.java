2049049df41a119260b751bc71ef9df30a4c7bbb
Antonio Fabregat <fabregat.antonio@gmail.com>
2018-04-25 11:19:02 +0100

    schemaDataSetTest uses google validator

--- src/test/java/org/reactome/server/graph/service/SchemaDataSetTest.java
+++ src/test/java/org/reactome/server/graph/service/SchemaDataSetTest.java
@@ -1,7 +1,7 @@
 package org.reactome.server.graph.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.io.IOUtils;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -10,16 +10,28 @@ import org.reactome.server.graph.domain.schema.SchemaDataSet;
 import org.reactome.server.graph.util.DatabaseObjectFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.lang.reflect.InvocationTargetException;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assume.assumeTrue;
 
 /**
+ * The documentation for schema validation is at https://developers.google.com/search/docs/guides/prototype
+ *
  * @author Antonio Fabregat <fabregat@ebi.ac.uk>
  */
 public class SchemaDataSetTest extends BaseTest {
 
-    private static final String stId = "R-HSA-446203";
+    private static final String stId = "R-HSA-6807070";
 
     @Autowired
     private DatabaseObjectService dbs;
@@ -48,7 +60,7 @@ public class SchemaDataSetTest extends BaseTest {
     }
 
     @Test
-    public void schemaDataSetTest() throws InvocationTargetException, IllegalAccessException {
+    public void schemaDataSetTest() {
 
         logger.info("Testing schema.org DataSet.");
 
@@ -58,8 +70,33 @@ public class SchemaDataSetTest extends BaseTest {
         ObjectMapper mapper = new ObjectMapper();
         try {
             String json = mapper.writeValueAsString(schemaDataSet);
-            System.out.println(json);
-        } catch (JsonProcessingException e) {
+
+            URL url = new URL("https://search.google.com/structured-data/testing-tool/validate");
+            URLConnection con = url.openConnection();
+            HttpURLConnection http = (HttpURLConnection) con;
+            http.setRequestMethod("POST");
+            http.setDoOutput(true);
+
+            Map<String, String> arguments = new HashMap<>();
+            arguments.put("html", json);
+            StringJoiner sj = new StringJoiner("&");
+            for (Map.Entry<String, String> entry : arguments.entrySet()) {
+                sj.add(URLEncoder.encode(entry.getKey(), "UTF-8") + "=" + URLEncoder.encode(entry.getValue(), "UTF-8"));
+            }
+            byte[] out = sj.toString().getBytes(StandardCharsets.UTF_8);
+            int length = out.length;
+
+            http.setFixedLengthStreamingMode(length);
+            http.setRequestProperty("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+            http.connect();
+            try (OutputStream os = http.getOutputStream()) {
+                os.write(out);
+            }
+
+            String response = IOUtils.toString(http.getInputStream(), "UTF-8");
+            Boolean valid = response.contains("\"totalNumErrors\":0,\"totalNumWarnings\":0");
+            assertTrue("The generated schema should not have errors neither warnings", valid);
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
