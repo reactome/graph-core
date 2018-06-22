422e14d805d4e36b21fb5f2767dbd47da2846fa5
Antonio Fabregat <fabregat.antonio@gmail.com>
2018-06-22 11:38:00 +0100

    cypher queries tuning

--- src/main/java/org/reactome/server/graph/repository/PersonRepository.java
+++ src/main/java/org/reactome/server/graph/repository/PersonRepository.java
@@ -17,46 +17,38 @@ import java.util.Collection;
 @Repository
 public interface PersonRepository extends GraphRepository<Person>{
 
-    @Query("MATCH (n:Person)-[r]->(m) WHERE n.surname in {0} AND n.firstname in {0} RETURN n, r, m")
+    @Query("MATCH (n:Person)-[r]->(m) WHERE n.surname in {0} AND n.firstname in {0} AND NOT (m:InstanceEdit) RETURN n, r, m")
     Collection<Person> findPersonByName(String[] name);
 
-    @Query("MATCH (n:Person)-[r]->(m) WHERE n.surname in {0} OR n.firstname in {0} RETURN n, r, m")
+    @Query("MATCH (n:Person)-[r]->(m) WHERE n.surname in {0} OR n.firstname in {0} AND NOT (m:InstanceEdit) RETURN n, r, m")
     Collection<Person> queryPersonByName(String[] name);
 
-    @Query("MATCH (n:Person{orcidId:{0}})-[r]->(m) Return n, r, m")
+    @Query("MATCH (n:Person{orcidId:{0}})-[r]->(m) WHERE NOT (m:InstanceEdit) RETURN n, r, m")
     Person findPersonByOrcidId(String orcidId);
 
-    @Query("MATCH (n:Person{dbId:{0}})-[r]->(m) Return n, r, m")
+    @Query("MATCH (n:Person{dbId:{0}})-[r]->(m) WHERE NOT (m:InstanceEdit) RETURN n, r, m")
     Person findPersonByDbId(Long dbId);
 
-    @Query("MATCH (n:Person{eMailAddress:{0}})-[r]->(m) Return n, r, m")
-    @Deprecated
-    Person findPersonByEmail(String email);
-
-    @Query("MATCH (n:Person{orcidId:{0}})-[:author]-(m:Publication) Return m")
+    @Query("MATCH (:Person{orcidId:{0}})-[:author]-(pub:Publication) RETURN pub")
     Collection<Publication> getPublicationsOfPersonByOrcidId(String orcidId);
 
-    @Query("MATCH (n:Person{dbId:{0}})-[:author]-(m:Publication) Return m")
+    @Query("MATCH (:Person{dbId:{0}})-[:author]-(pub:Publication) RETURN pub")
     Collection<Publication> getPublicationsOfPersonByDbId(Long dbId);
 
-    @Query("MATCH (n:Person{eMailAddress:{0}})-[:author]-(m:Publication) Return m")
+    @Query("MATCH (:Person{eMailAddress:{0}})-[:author]-(pub:Publication) RETURN pub")
     Collection<Publication> getPublicationsOfPersonByEmail(String email);
 
-    @Query("MATCH (n:Person{orcidId:{0}})-[r:author]->(m:InstanceEdit)-[e:authored]->(k:Pathway) RETURN k")
+    @Query("MATCH (:Person{orcidId:{0}})-[:author]->(:InstanceEdit)-[:authored]->(p:Pathway) RETURN p")
     Collection<Pathway> getAuthoredPathwaysByOrcidId(String orcidId);
 
-    @Query("MATCH (n:Person{dbId:{0}})-[r:author]->(m:InstanceEdit)-[e:authored]->(k:Pathway) RETURN k")
+    @Query("MATCH (:Person{dbId:{0}})-[:author]->(:InstanceEdit)-[:authored]->(p:Pathway) RETURN p")
     Collection<Pathway> getAuthoredPathwaysByDbId(Long dbId);
 
-    @Query("MATCH (n:Person{orcidId:{0}})-[r:author]->(m:InstanceEdit)-[e:reviewed]->(k:Pathway) RETURN k")
+    @Query("MATCH (:Person{orcidId:{0}})-[:author]->(:InstanceEdit)-[:reviewed]->(p:Pathway) RETURN p")
     Collection<Pathway> getReviewedPathwaysByOrcidId(String orcidId);
 
-    @Query("MATCH (n:Person{dbId:{0}})-[r:author]->(m:InstanceEdit)-[e:reviewed]->(k:Pathway) RETURN k")
+    @Query("MATCH (:Person{dbId:{0}})-[:author]->(:InstanceEdit)-[:reviewed]->(p:Pathway) RETURN p")
     Collection<Pathway> getReviewedPathwaysByDbId(Long dbId);
-
-    @Query("MATCH (n:Person{eMailAddress:{0}})-[r:author]->(m:InstanceEdit)-[e:authored]->(k:Pathway) RETURN k")
-    @Deprecated
-    Collection<Pathway> getAuthoredPathwaysByEmail(String email);
 
     @Query(" MATCH (per:Person) " +
             "OPTIONAL MATCH (per)-[:author|authored*]->(ap:Pathway) " +
