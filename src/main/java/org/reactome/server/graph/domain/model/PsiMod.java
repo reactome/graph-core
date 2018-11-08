dd7f76c9f500f43233c5c96b73c078087d71256c
Antonio Fabregat <fabregat.antonio@gmail.com>
2018-11-08 09:16:07 +0000

    abbreviation field added

--- src/main/java/org/reactome/server/graph/domain/model/PsiMod.java
+++ src/main/java/org/reactome/server/graph/domain/model/PsiMod.java
@@ -1,10 +1,24 @@
 package org.reactome.server.graph.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.neo4j.ogm.annotation.NodeEntity;
+import org.reactome.server.graph.domain.annotations.ReactomeProperty;
 
 @SuppressWarnings("unused")
 @NodeEntity
 public class PsiMod extends ExternalOntology {
 
+    @JsonIgnore
+    @ReactomeProperty(addedField = true)
+    private String abbreviation;
+
     public PsiMod() {}
+
+    public String getAbbreviation() {
+        return abbreviation;
+    }
+
+    public void setAbbreviation(String abbreviation) {
+        this.abbreviation = abbreviation;
+    }
 }
