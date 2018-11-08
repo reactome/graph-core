5d0e876ce732b904438ebec29268527c7c358399
Antonio Fabregat <fabregat.antonio@gmail.com>
2018-11-08 09:15:37 +0000

    hasEvent removed

--- src/main/java/org/reactome/server/graph/domain/model/BlackBoxEvent.java
+++ src/main/java/org/reactome/server/graph/domain/model/BlackBoxEvent.java
@@ -4,8 +4,6 @@ import org.neo4j.ogm.annotation.NodeEntity;
 import org.neo4j.ogm.annotation.Relationship;
 import org.reactome.server.graph.domain.annotations.ReactomeSchemaIgnore;
 
-import java.util.List;
-
 /**
  * Shortcut reactions that make the connection between input and output, but don't provide complete mechanistic detail. Used for reactions that do not balance, or complicated processes for which we either don't know all the details, or we choose not to represent every step. (e.g. degradation of a protein)
  */
@@ -21,22 +19,10 @@ public class BlackBoxEvent extends ReactionLikeEvent {
 
     }
 
-    @Relationship(type = "hasEvent")
-    private List<Event> hasEvent;
-
     @Relationship(type = "templateEvent")
     private Event templateEvent;
 
     public BlackBoxEvent() {}
-
-    public List<Event> getHasEvent() {
-        return hasEvent;
-    }
-
-    @Relationship(type = "hasEvent")
-    public void setHasEvent(List<Event> hasEvent) {
-        this.hasEvent = hasEvent;
-    }
 
     public Event getTemplateEvent() {
         return templateEvent;
