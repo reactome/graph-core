aaeabffbaa67d449e645716c3a4e35eb215124b0
Antonio Fabregat <fabregat.antonio@gmail.com>
2019-02-03 16:39:42 +0000

    cleanup

--- src/main/java/org/reactome/server/graph/repository/ParticipantRepository.java
+++ src/main/java/org/reactome/server/graph/repository/ParticipantRepository.java
@@ -34,7 +34,7 @@ public interface ParticipantRepository extends GraphRepository<PhysicalEntity> {
 
     @Query(" MATCH (n:DatabaseObject{dbId:{0}})-[:hasEvent|input|output|catalystActivity|physicalEntity|entityFunctionalStatus|diseaseEntity|regulatedBy|regulator*]->(m:PhysicalEntity) " +
             "OPTIONAL MATCH (m)-[:referenceEntity]->(re1:ReferenceEntity) " +
-            "OPTIONAL MATCH (m)-[:physicalEntity|hasMember|hasComponent|hasCandidate|repeatedUnit*]->(pe:PhysicalEntity)-[:referenceEntity]->(re2:ReferenceEntity) " +
+            "OPTIONAL MATCH (m)-[:hasMember|hasComponent|hasCandidate|repeatedUnit*]->(pe:PhysicalEntity)-[:referenceEntity]->(re2:ReferenceEntity) " +
             "WITH DISTINCT m,  " +
             "              CASE WHEN re1 IS NULL THEN re2 ELSE re1 END AS re, " +
             "              CASE WHEN pe IS NULL THEN m.schemaClass ELSE pe.schemaClass END AS icon " +
@@ -54,7 +54,7 @@ public interface ParticipantRepository extends GraphRepository<PhysicalEntity> {
 
     @Query(" MATCH (n:DatabaseObject{stId:{0}})-[:hasEvent|input|output|catalystActivity|physicalEntity|entityFunctionalStatus|diseaseEntity|regulatedBy|regulator*]->(m:PhysicalEntity) " +
             "OPTIONAL MATCH (m)-[:referenceEntity]->(re1:ReferenceEntity) " +
-            "OPTIONAL MATCH (m)-[:physicalEntity|hasMember|hasComponent|hasCandidate|repeatedUnit*]->(pe:PhysicalEntity)-[:referenceEntity]->(re2:ReferenceEntity) " +
+            "OPTIONAL MATCH (m)-[:hasMember|hasComponent|hasCandidate|repeatedUnit*]->(pe:PhysicalEntity)-[:referenceEntity]->(re2:ReferenceEntity) " +
             "WITH DISTINCT m, " +
             "              CASE WHEN re1 IS NULL THEN re2 ELSE re1 END AS re, " +
             "              CASE WHEN pe IS NULL THEN m.schemaClass ELSE pe.schemaClass END AS icon " +
