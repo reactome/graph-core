5b843d8a5590dd481f86c6ab98b79ea8e3e68252
Antonio Fabregat <fabregat.antonio@gmail.com>
2019-04-17 10:57:48 +0100

    v69 fields spring cleanup

--- src/main/java/org/reactome/server/graph/domain/model/GO_MolecularFunction.java
+++ src/main/java/org/reactome/server/graph/domain/model/GO_MolecularFunction.java
@@ -1,11 +1,7 @@
 package org.reactome.server.graph.domain.model;
 
 import org.neo4j.ogm.annotation.NodeEntity;
-import org.neo4j.ogm.annotation.Relationship;
-import org.reactome.server.graph.domain.annotations.ReactomeAllowedClasses;
 import org.reactome.server.graph.domain.annotations.ReactomeProperty;
-
-import java.util.List;
 
 @SuppressWarnings("unused")
 @NodeEntity
@@ -13,29 +9,6 @@ public class GO_MolecularFunction extends GO_Term {
 
     @ReactomeProperty
     private String ecNumber;
-
-    @Relationship(type = "componentOf")
-    @ReactomeAllowedClasses(allowed = {GO_BiologicalProcess.class, GO_MolecularFunction.class})
-    private List<GO_Term> componentOf;
-
-    @Relationship(type = "hasPart")
-    @ReactomeAllowedClasses(allowed = {GO_BiologicalProcess.class, GO_MolecularFunction.class})
-    private List<GO_Term> hasPart;
-
-    @Relationship(type = "instanceOf")
-    private List<GO_MolecularFunction> instanceOf;
-
-    @Relationship(type = "negativelyRegulate")
-    @ReactomeAllowedClasses(allowed = {GO_BiologicalProcess.class, GO_MolecularFunction.class})
-    private List<GO_Term> negativelyRegulate;
-
-    @Relationship(type = "positivelyRegulate")
-    @ReactomeAllowedClasses(allowed = {GO_BiologicalProcess.class, GO_MolecularFunction.class})
-    private List<GO_Term> positivelyRegulate;
-
-    @Relationship(type = "regulate")
-    @ReactomeAllowedClasses(allowed = {GO_BiologicalProcess.class, GO_MolecularFunction.class})
-    private List<GO_Term> regulate;
 
     public GO_MolecularFunction() {}
 
@@ -48,62 +21,4 @@ public class GO_MolecularFunction extends GO_Term {
         this.ecNumber = ecNumber;
     }
 
-    @ReactomeAllowedClasses(allowed = {GO_BiologicalProcess.class, GO_MolecularFunction.class})
-    public List<GO_Term> getComponentOf() {
-        return componentOf;
-    }
-
-    @Relationship(type = "componentOf")
-    public void setComponentOf(List<GO_Term> componentOf) {
-        this.componentOf = componentOf;
-    }
-
-    @ReactomeAllowedClasses(allowed = {GO_BiologicalProcess.class, GO_MolecularFunction.class})
-    public List<GO_Term> getHasPart() {
-        return hasPart;
-    }
-
-    @Relationship(type = "hasPart")
-    public void setHasPart(List<GO_Term> hasPart) {
-        this.hasPart = hasPart;
-    }
-
-    public List<GO_MolecularFunction> getInstanceOf() {
-        return instanceOf;
-    }
-
-    @Relationship(type = "instanceOf")
-    public void setInstanceOf(List<GO_MolecularFunction> instanceOf) {
-        this.instanceOf = instanceOf;
-    }
-
-    @ReactomeAllowedClasses(allowed = {GO_BiologicalProcess.class, GO_MolecularFunction.class})
-    public List<GO_Term> getNegativelyRegulate() {
-        return negativelyRegulate;
-    }
-
-    @Relationship(type = "negativelyRegulate")
-    public void setNegativelyRegulate(List<GO_Term> negativelyRegulate) {
-        this.negativelyRegulate = negativelyRegulate;
-    }
-
-    @ReactomeAllowedClasses(allowed = {GO_BiologicalProcess.class, GO_MolecularFunction.class})
-    public List<GO_Term> getPositivelyRegulate() {
-        return positivelyRegulate;
-    }
-
-    @Relationship(type = "positivelyRegulate")
-    public void setPositivelyRegulate(List<GO_Term> positivelyRegulate) {
-        this.positivelyRegulate = positivelyRegulate;
-    }
-
-    @ReactomeAllowedClasses(allowed = {GO_BiologicalProcess.class, GO_MolecularFunction.class})
-    public List<GO_Term> getRegulate() {
-        return regulate;
-    }
-
-    @Relationship(type = "regulate")
-    public void setRegulate(List<GO_Term> regulate) {
-        this.regulate = regulate;
-    }
 }
