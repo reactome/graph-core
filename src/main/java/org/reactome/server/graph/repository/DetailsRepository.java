6548c0977962fe7ded1706345c7344e11c13eed2
Guilherme Viteri <gviteri@ebi.ac.uk>
2021-05-21 09:50:40 +0100

    Migrating to SDN 6  - work in progress  - DetailsRepository adding by stId query method.

--- src/main/java/org/reactome/server/graph/repository/DetailsRepository.java
+++ src/main/java/org/reactome/server/graph/repository/DetailsRepository.java
@@ -7,12 +7,6 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Map;
 
-/**
- * Created by:
- *
- * @author Florian Korninger (florian.korninger@ebi.ac.uk)
- * @since 14.04.16.
- */
 @Repository
 public class DetailsRepository {
 
@@ -23,41 +17,22 @@ public class DetailsRepository {
         this.neo4jTemplate = neo4jTemplate;
     }
 
-    //    public DatabaseObject detailsPageQuery(String stId) {
-//        String query = "MATCH (n:DatabaseObject{stId:{stId}})-[r]->(m)" +
-//                "OPTIONAL MATCH (n)<-[e:inferredTo|regulator|regulatedBy]-(l)" +
-//                "OPTIONAL MATCH (m:ReferenceEntity)-[t:crossReference|referenceGene|referenceTranscript]->(z)" +
-//                "OPTIONAL MATCH (m:AbstractModifiedResidue)-[u:psiMod|modification]-(i)" +
-//                "OPTIONAL MATCH (m:CatalystActivity)-[o:catalystActivity|physicalEntity|activity]-(p)" +
-//                "OPTIONAL MATCH (m:EntityFunctionalStatus)-[q:diseaseEntity|normalEntity|functionalStatus]-(s)-[a:functionalStatusType|structuralVariant]-(b)" +
-//                "RETURN n,r,m,l,e,t,z,u,i,o,p,q,s,a,b";
-//        Map<String, Object> map = new HashMap<>();
-//        map.put("stId", stId);
-//        Result result = neo4jTemplate.query(query, map);
-//        if (result != null && result.iterator().hasNext()) {
-//            DatabaseObject n = (DatabaseObject) result.iterator().next().get("n");
-//            return n;
-//        }
-//        return null;
-//    }
-//
-//    public DatabaseObject detailsPageQuery(Long dbId) {
-//        String query = "MATCH (n:DatabaseObject{dbId:{dbId}})-[r]->(m)" +
-//                "OPTIONAL MATCH (n)<-[e:inferredTo|regulator|regulatedBy]-(l)" +
-//                "OPTIONAL MATCH (m:ReferenceEntity)-[t:crossReference|referenceGene|referenceTranscript]->(z)" +
-//                "OPTIONAL MATCH (m:AbstractModifiedResidue)-[u:psiMod|modification]-(i)" +
-//                "OPTIONAL MATCH (m:CatalystActivity)-[o:catalystActivity|physicalEntity|activity]-(p)" +
-//                "OPTIONAL MATCH (m:EntityFunctionalStatus)-[q:diseaseEntity|normalEntity|functionalStatus]-(s)-[a:functionalStatusType|structuralVariant]-(b)" +
-//                "RETURN n,r,m,l,e,t,z,u,i,o,p,q,s,a,b";
-//        Map<String, Object> map = new HashMap<>();
-//        map.put("dbId", dbId);
-//        Result result = neo4jTemplate.query(query, map);
-//        if (result != null && result.iterator().hasNext())
-//            return (DatabaseObject) result.iterator().next().get("n");
-//        return null;
-//    }
+    public DatabaseObject detailsPageQuery(String stId) {
+        //language=Cypher
+        String query = "" +
+                "MATCH (n:DatabaseObject{dbId:$stId})-[r]->(m)" +
+                "OPTIONAL MATCH (n)<-[e:inferredTo|regulator|regulatedBy]-(l)" +
+                "OPTIONAL MATCH (m:ReferenceEntity)-[t:crossReference|referenceGene|referenceTranscript]->(z)" +
+                "OPTIONAL MATCH (m:AbstractModifiedResidue)-[u:psiMod|modification]-(i)" +
+                "OPTIONAL MATCH (m:CatalystActivity)-[o:catalystActivity|physicalEntity|activity]-(p)" +
+                "OPTIONAL MATCH (m:EntityFunctionalStatus)-[q:diseaseEntity|normalEntity|functionalStatus]-(s)-[a:functionalStatusType|structuralVariant]-(b)" +
+                "RETURN n, COLLECT(r), COLLECT(m),COLLECT(l),COLLECT(e),COLLECT(t),COLLECT(z),COLLECT(u),COLLECT(i),COLLECT(o),COLLECT(p),COLLECT(q),COLLECT(s),COLLECT(a),COLLECT(b)";
+
+        return neo4jTemplate.findOne(query, Map.of("stId", stId), DatabaseObject.class).orElse(null);
+    }
 
     public DatabaseObject detailsPageQuery(Long dbId) {
+        //language=Cypher
         String query = "" +
                 "MATCH (n:DatabaseObject{dbId:$dbId})-[r]->(m)" +
                 "OPTIONAL MATCH (n)<-[e:inferredTo|regulator|regulatedBy]-(l)" +
@@ -65,11 +40,9 @@ public class DetailsRepository {
                 "OPTIONAL MATCH (m:AbstractModifiedResidue)-[u:psiMod|modification]-(i)" +
                 "OPTIONAL MATCH (m:CatalystActivity)-[o:catalystActivity|physicalEntity|activity]-(p)" +
                 "OPTIONAL MATCH (m:EntityFunctionalStatus)-[q:diseaseEntity|normalEntity|functionalStatus]-(s)-[a:functionalStatusType|structuralVariant]-(b)" +
-                "RETURN n,collect(r),collect(m),collect(l),collect(e),collect(t),collect(z),collect(u),collect(i),collect(o),collect(p),collect(q),collect(s),collect(a),collect(b)";
+                "RETURN n, COLLECT(r), COLLECT(m),COLLECT(l),COLLECT(e),COLLECT(t),COLLECT(z),COLLECT(u),COLLECT(i),COLLECT(o),COLLECT(p),COLLECT(q),COLLECT(s),COLLECT(a),COLLECT(b)";
 
-        Map<String, Object> map = Map.of("dbId", dbId);
-        return neo4jTemplate.findOne(query, map, DatabaseObject.class).orElse(null);
+        return neo4jTemplate.findOne(query, Map.of("dbId", dbId), DatabaseObject.class).orElse(null);
     }
-
 }
 
