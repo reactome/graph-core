b5e791475ab44dd84e15ace85df2a6d269970513
EliotRagueneau <159753.AZErty>
2021-06-21 14:27:39 +0100

    :wrench: Use prong-boot-starter-aop

--- src/main/java/org/reactome/server/graph/config/GraphCoreNeo4jConfig.java
+++ src/main/java/org/reactome/server/graph/config/GraphCoreNeo4jConfig.java
@@ -2,14 +2,12 @@ package org.reactome.server.graph.config;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @ComponentScan(basePackages = "org.reactome.server.graph")
 @EnableNeo4jRepositories(basePackages = "org.reactome.server.graph.repository")
 @EnableTransactionManagement
-@EnableSpringConfigured
 @EnableAutoConfiguration
 public class GraphCoreNeo4jConfig {
 }
