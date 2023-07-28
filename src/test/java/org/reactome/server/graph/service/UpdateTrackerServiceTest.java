829d77d38fe2f39e82fa12b20978254666ba4ee5
cqgong <cgong@ebi.ac.uk>
2023-07-28 13:20:37 +0100

    :white_check_mark: create @Test for metaDataBaseObject

--- src/test/java/org/reactome/server/graph/service/UpdateTrackerServiceTest.java
+++ src/test/java/org/reactome/server/graph/service/UpdateTrackerServiceTest.java
@@ -1,9 +1,9 @@
 package org.reactome.server.graph.service;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.reactome.server.graph.domain.model.Complex;
-import org.reactome.server.graph.domain.model.Reaction;
+import org.reactome.server.graph.domain.model.Pathway;
 import org.reactome.server.graph.domain.model.Trackable;
 import org.reactome.server.graph.domain.model.UpdateTracker;
 import org.reactome.server.graph.util.DatabaseObjectFactory;
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class UpdateTrackerServiceTest extends BaseTest {
@@ -36,32 +35,29 @@ public class UpdateTrackerServiceTest extends BaseTest {
     UpdateTrackerService service;
 
     @Test
-    void testFindById() {
-        Optional<UpdateTracker> updateTracker = this.service.findById(999999999999999L);
-        assertTrue(updateTracker.isPresent());
+    void testFindUpdateTrackerByDbId() {
+
+        Optional<UpdateTracker> updateTracker = this.service.findUpdateTrackerByDbId(9776257L);
+        Assertions.assertNotNull(updateTracker);
+
+        Assertions.assertEquals(77, updateTracker.get().getRelease().getReleaseNumber());
+
         List<Trackable> updatedInstances = updateTracker.get().getUpdatedInstance();
-        assertEquals(2, updatedInstances.size());
-        assertEquals(77, updateTracker.get().getRelease().getReleaseNumber());
-        assertTrue(updatedInstances.stream().anyMatch(trackable -> trackable instanceof Reaction));
-        assertTrue(updatedInstances.stream().anyMatch(trackable -> trackable instanceof Complex));
+
+        Assertions.assertTrue(!updatedInstances.isEmpty());
+        Assertions.assertTrue(updatedInstances.stream().anyMatch(trackable -> trackable instanceof Pathway));
     }
 
-    @Test
-    void testFindByReleaseNumber() {
-        List<UpdateTracker> updateTracker = this.service.findByReleaseNumber(77);
-        assertTrue(updateTracker.size() >= 1);
-    }
 
     @Test
     void testFindByUpdatedInstanceDbId() {
-        List<UpdateTracker> updateTracker = this.service.findByUpdatedInstanceDbId(6793661L);
+        List<UpdateTracker> updateTracker = this.service.findByUpdatedInstanceDbId(3299685L);
         assertTrue(updateTracker.size() >= 1);
     }
 
     @Test
     void testFindByUpdatedInstanceStId() {
-        List<UpdateTracker> updateTracker = this.service.findByUpdatedInstanceStId("R-HSA-8944365");
+        List<UpdateTracker> updateTracker = this.service.findByUpdatedInstanceStId("R-HSA-3299685");
         assertTrue(updateTracker.size() >= 1);
     }
-
 }
