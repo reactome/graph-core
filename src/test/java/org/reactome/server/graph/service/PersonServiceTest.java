3a626a558666c109b2b7b01718bf3724e030d1cd
agrent <alexander.michael.grentner@gmail.com>
2025-02-19 15:28:06 +0000

    TR PathwaysService, PersonServiceTest

--- src/test/java/org/reactome/server/graph/service/PersonServiceTest.java
+++ src/test/java/org/reactome/server/graph/service/PersonServiceTest.java
@@ -33,7 +33,7 @@ public class PersonServiceTest extends BaseTest {
         logger.info("Started testing personService.findPersonByName");
         long start, time;
         start = System.currentTimeMillis();
-        Collection<Person> persons = personService.findPersonByName("steve jupe");
+        Collection<Person> persons = personService.findPersonByName(testPerson.getDisplayName());
         time = System.currentTimeMillis() - start;
         logger.info("GraphDb execution time: " + time + "ms");
 
@@ -46,11 +46,11 @@ public class PersonServiceTest extends BaseTest {
         logger.info("Started testing personService.queryPersonByName");
         long start, time;
         start = System.currentTimeMillis();
-        Collection<Person> persons = personService.queryPersonByName("steve jupe");
+        Collection<Person> persons = personService.queryPersonByName(testPerson.getDisplayName());
         time = System.currentTimeMillis() - start;
         logger.info("GraphDb execution time: " + time + "ms");
 
-        assertTrue(persons.size() >= 14);
+        assertEquals(1, persons.size());
         logger.info("Finished");
     }
 
@@ -59,11 +59,11 @@ public class PersonServiceTest extends BaseTest {
         logger.info("Started testing personService.findPersonByOrcidIdTest");
         long start, time;
         start = System.currentTimeMillis();
-        Person person = personService.findPerson("0000-0001-5807-0069");
+        Person person = personService.findPerson(testPerson.getOrcidId());
         time = System.currentTimeMillis() - start;
         logger.info("GraphDb execution time: " + time + "ms");
 
-        assertEquals("Jupe, S", person.getDisplayName());
+        assertEquals(testPerson.getDisplayName(), person.getDisplayName());
         logger.info("Finished");
     }
 
@@ -72,12 +72,11 @@ public class PersonServiceTest extends BaseTest {
         logger.info("Started testing personService.findPersonByDbIdTest");
         long start, time;
         start = System.currentTimeMillis();
-        Person person = personService.findPerson(217030L);
+        Person person = personService.findPerson(testPerson.getDbId());
         time = System.currentTimeMillis() - start;
         logger.info("GraphDb execution time: " + time + "ms");
 
-        assertEquals("Takao, N", person.getDisplayName());
-        assertFalse(person.getPublications().isEmpty());
+        assertEquals(testPerson.getDisplayName(), person.getDisplayName());
         logger.info("Finished");
     }
 
@@ -86,11 +85,11 @@ public class PersonServiceTest extends BaseTest {
         logger.info("Started testing personService.getPublicationsOfPersonByOrcidId");
         long start, time;
         start = System.currentTimeMillis();
-        Collection<Publication> publications = personService.getPublicationsOfPerson("0000-0001-5807-0069");
+        Collection<Publication> publications = personService.getPublicationsOfPerson(testPerson.getOrcidId());
         time = System.currentTimeMillis() - start;
         logger.info("GraphDb execution time: " + time + "ms");
 
-        assertEquals(3, publications.size());
+        assertEquals(1, publications.size());
         logger.info("Finished");
     }
 
@@ -99,11 +98,11 @@ public class PersonServiceTest extends BaseTest {
         logger.info("Started testing personService.getPublicationsOfPersonByDbId");
         long start, time;
         start = System.currentTimeMillis();
-        Collection<Publication> publications = personService.getPublicationsOfPerson(391309L);
+        Collection<Publication> publications = personService.getPublicationsOfPerson(testPerson.getDbId());
         time = System.currentTimeMillis() - start;
         logger.info("GraphDb execution time: " + time + "ms");
 
-        assertEquals(3, publications.size());
+        assertEquals(1, publications.size());
         logger.info("Finished");
     }
 
@@ -112,11 +111,11 @@ public class PersonServiceTest extends BaseTest {
         logger.info("Started testing personService.getAuthoredPathwaysByOrcidId");
         long start, time;
         start = System.currentTimeMillis();
-        Collection<SimpleEventProjection> pathways = personService.getAuthoredPathways("0000-0001-5807-0069");
+        Collection<SimpleEventProjection> pathways = personService.getAuthoredPathways(testPerson.getOrcidId());
         time = System.currentTimeMillis() - start;
         logger.info("GraphDb execution time: " + time + "ms");
 
-        assertTrue(pathways.size() >= 65);
+        assertTrue(!pathways.isEmpty());
         logger.info("Finished");
     }
 
@@ -125,37 +124,39 @@ public class PersonServiceTest extends BaseTest {
         logger.info("Started testing personService.getAuthoredPathwaysByDbId");
         long start, time;
         start = System.currentTimeMillis();
-        Collection<SimpleEventProjection> pathways = personService.getAuthoredPathways(391309L);
+        Collection<SimpleEventProjection> pathways = personService.getAuthoredPathways(testPerson.getDbId());
         time = System.currentTimeMillis() - start;
         logger.info("GraphDb execution time: " + time + "ms");
 
-        assertTrue(pathways.size() >= 65);
+        assertTrue(!pathways.isEmpty());
         logger.info("Finished");
     }
+
 
     @Test
     public void getAuthoredReactionsByOrcidIdTest() {
         logger.info("Started testing personService.getAuthoredReactionsByOrcidId");
         long start, time;
         start = System.currentTimeMillis();
-        Collection<SimpleEventProjection> reactions = personService.getAuthoredReactions("0000-0001-5193-0855");
+        Collection<SimpleEventProjection> reactions = personService.getAuthoredReactions(testPerson.getOrcidId());
         time = System.currentTimeMillis() - start;
         logger.info("GraphDb execution time: " + time + "ms");
 
-        assertTrue(reactions.size() >= 1495);
+        assertTrue(!reactions.isEmpty());
         logger.info("Finished");
     }
+
 
     @Test
     public void getAuthoredReactionsByDbIdTest() {
         logger.info("Started testing personService.getAuthoredReactionsByDbId");
         long start, time;
         start = System.currentTimeMillis();
-        Collection<SimpleEventProjection> pathways = personService.getAuthoredReactions(203835L);
+        Collection<SimpleEventProjection> pathways = personService.getAuthoredReactions(testPerson.getDbId());
         time = System.currentTimeMillis() - start;
         logger.info("GraphDb execution time: " + time + "ms");
 
-        assertTrue(pathways.size() >= 1495);
+        assertTrue(!pathways.isEmpty());
         logger.info("Finished");
     }
 
@@ -164,11 +165,11 @@ public class PersonServiceTest extends BaseTest {
         logger.info("Started testing personService.getReviewedPathwaysByOrcidId");
         long start, time;
         start = System.currentTimeMillis();
-        Collection<SimpleEventProjection> pathways = personService.getReviewedPathways("0000-0001-5807-0069");
+        Collection<SimpleEventProjection> pathways = personService.getReviewedPathways(testPerson.getOrcidId());
         time = System.currentTimeMillis() - start;
         logger.info("GraphDb execution time: " + time + "ms");
 
-        assertTrue(pathways.size() >= 14);
+        assertTrue(!pathways.isEmpty());
         logger.info("Finished");
     }
 
@@ -177,11 +178,11 @@ public class PersonServiceTest extends BaseTest {
         logger.info("Started testing personService.getReviewedPathwaysByDbId");
         long start, time;
         start = System.currentTimeMillis();
-        Collection<SimpleEventProjection> pathways = personService.getReviewedPathways(391309L);
+        Collection<SimpleEventProjection> pathways = personService.getReviewedPathways(testPerson.getDbId());
         time = System.currentTimeMillis() - start;
         logger.info("GraphDb execution time: " + time + "ms");
 
-        assertTrue(pathways.size() >= 14);
+        assertTrue(!pathways.isEmpty());
         logger.info("Finished");
     }
 
@@ -190,11 +191,11 @@ public class PersonServiceTest extends BaseTest {
         logger.info("Started testing personService.getReviewedReactionsByOrcidId");
         long start, time;
         start = System.currentTimeMillis();
-        Collection<SimpleEventProjection> reactions = personService.getReviewedReactions("0000-0001-5193-0855");
+        Collection<SimpleEventProjection> reactions = personService.getReviewedReactions(testPerson.getOrcidId());
         time = System.currentTimeMillis() - start;
         logger.info("GraphDb execution time: " + time + "ms");
 
-        assertTrue(reactions.size() >= 19);
+        assertTrue(!reactions.isEmpty());
         logger.info("Finished");
     }
 
@@ -203,11 +204,11 @@ public class PersonServiceTest extends BaseTest {
         logger.info("Started testing personService.getReviewedReactionsByDbId");
         long start, time;
         start = System.currentTimeMillis();
-        Collection<SimpleEventProjection> reactions = personService.getReviewedReactions(203835L);
+        Collection<SimpleEventProjection> reactions = personService.getReviewedReactions(testPerson.getDbId());
         time = System.currentTimeMillis() - start;
         logger.info("GraphDb execution time: " + time + "ms");
 
-        assertTrue(reactions.size() >= 19);
+        assertTrue(!reactions.isEmpty());
         logger.info("Finished");
     }
 
@@ -220,7 +221,6 @@ public class PersonServiceTest extends BaseTest {
         time = System.currentTimeMillis() - start;
         logger.info("GraphDb execution time: " + time + "ms");
         assertNotNull(objs.iterator().next().getPerson());
-        assertTrue(objs.size() >= 600);
         logger.info("Finished");
     }
 
