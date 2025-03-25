1092202d855b2b72b463af4e930a4768ee35b02a
agrentner <alexander.michael.grentner@gmail.com>
2025-03-25 16:53:32 +0000

    Deployment Script Update

--- update_major_version.sh
+++ update_major_version.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+PACKAGE_NAME=graph.core
+
 # Get the current project version using Maven
 current_version=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
 echo "Current version: $current_version"
@@ -14,3 +16,9 @@ echo "New version: $new_version"
 
 # Set the new version using Maven
 mvn versions:set -DnewVersion=$new_version
+
+if grep -q "^${PACKAGE_NAME}=" /var/go/versions.properties; then
+    sed -i "s/^${PACKAGE_NAME}=.*/${PACKAGE_NAME}=${new_version}/" /var/go/versions.properties
+else
+    echo "${PACKAGE_NAME}=${new_version}" >> /var/go/versions.properties
+fi
