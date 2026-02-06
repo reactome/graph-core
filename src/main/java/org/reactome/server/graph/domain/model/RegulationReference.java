7a8de5a79ca8ca94e6871ffe95451a6352825e48
guanmingwu <guanmingwu@gmail.com>
2026-02-06 14:40:12 -0800

    added back regulation to RegulationReference

--- src/main/java/org/reactome/server/graph/domain/model/RegulationReference.java
+++ src/main/java/org/reactome/server/graph/domain/model/RegulationReference.java
@@ -3,6 +3,9 @@ package org.reactome.server.graph.domain.model;
 import org.springframework.data.neo4j.core.schema.Node;
 import org.springframework.data.neo4j.core.schema.Relationship;
 
+//NB by GW: I am not sure why the original code needs to use regulatedBy, intead of regulation. To make
+// it compatible with the original code and also be matched with the data model, I will keep both sets
+// of getter and setter. 
 @Node
 public class RegulationReference extends ControlReference {
 
@@ -19,4 +22,14 @@ public class RegulationReference extends ControlReference {
     public void setRegulatedBy(Regulation regulation) {
         this.regulation = regulation;
     }
+
+    public Regulation getRegulation() {
+        return regulation;
+    }
+
+    public void setRegulation(Regulation regulation) {
+        this.regulation = regulation;
+    }
+    
+    
 }
