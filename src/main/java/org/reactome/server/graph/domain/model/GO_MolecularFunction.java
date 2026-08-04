a90475f9c61dfb799539a26a8e3905f6137a50da
Joel Weiser <joel.weiser@oicr.on.ca>
2026-08-04 02:16:22 -0400

    changes ecNumber type to List of Strings

--- src/main/java/org/reactome/server/graph/domain/model/GO_MolecularFunction.java
+++ src/main/java/org/reactome/server/graph/domain/model/GO_MolecularFunction.java
@@ -3,21 +3,24 @@ package org.reactome.server.graph.domain.model;
 import org.reactome.server.graph.domain.annotations.ReactomeProperty;
 import org.springframework.data.neo4j.core.schema.Node;
 
+import java.util.List;
+
 @SuppressWarnings("unused")
 @Node
 public class GO_MolecularFunction extends GO_Term {
 
+    // Multi-valued in the Reactome schema: a GO molecular function can map to more than one EC number.
     @ReactomeProperty
-    private String ecNumber;
+    private List<String> ecNumber;
 
     public GO_MolecularFunction() {}
 
 
-    public String getEcNumber() {
+    public List<String> getEcNumber() {
         return ecNumber;
     }
 
-    public void setEcNumber(String ecNumber) {
+    public void setEcNumber(List<String> ecNumber) {
         this.ecNumber = ecNumber;
     }
 
